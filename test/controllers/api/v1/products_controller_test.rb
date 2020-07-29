93b3b69acd4eb9ec25c02795a5944d776b54b3d0
crazyoptimist <crazyoptimist@mail.com>
2020-07-29 02:18:29 -0500

    Add product create action

--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -18,4 +18,22 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_equal @product.title, json_response['title']
   end
 
+  test 'should create product' do
+    assert_difference('Product.count') do
+      post api_v1_products_url,
+        params: { product: { title: @product.title, price: @product.price, published: @product.published } },
+        headers: { Authorization: JsonWebToken.encode(user_id: @product.user_id) },
+        as: :json
+    end
+    assert_response :created
+  end
+
+  test 'should forbid create product' do
+    assert_no_difference('Product.count') do
+      post api_v1_products_url,
+        params: { product: { title: @product.title, price: @product.price, published: @product.published } },
+        as: :json
+    end
+    assert_response :forbidden
+  end
 end

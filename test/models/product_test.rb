ba880ae83a9c05607608593f067e08dd909be190
crazyoptimist <crazyoptimist@mail.com>
2020-08-02 21:23:53 -0500

    Add filter_by_title, filter_by_price

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -6,4 +6,23 @@ class ProductTest < ActiveSupport::TestCase
     product.price = -1
     assert_not product.valid?
   end
+
+  test "should filter products by name" do
+    assert_equal 2, Product.filter_by_title('tv').count
+  end
+
+  test 'should filter products by name and sort them' do
+    assert_equal [products(:another_tv), products(:one)],
+      Product.filter_by_title('tv').sort
+  end
+
+  test 'should filter products byt price and sort them' do
+    assert_equal [products(:two), products(:one)], 
+      Product.above_or_equal_to_price(200).sort
+  end
+
+  test 'should filter products by price lower and sort them' do
+    assert_equal [products(:another_tv)],
+      Product.below_or_equal_to_price(200).sort
+  end
 end

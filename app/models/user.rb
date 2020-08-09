677afdef6d84b573f385ffbe201e580c8156d8e2
crazyoptimist <crazyoptimist@mail.com>
2020-08-09 11:06:06 -0500

    :card_file_box:Generate orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_27_071344) do
+ActiveRecord::Schema.define(version: 2020_08_09_155929) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.decimal "total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -30,5 +38,6 @@ ActiveRecord::Schema.define(version: 2020_07_27_071344) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "users"
 end

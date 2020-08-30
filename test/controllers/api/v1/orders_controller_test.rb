40a4f6a90b8a6c69f1baf1b61f682d74a3d0754b
crazyoptimist <crazyoptimist@mail.com>
2020-08-29 23:00:18 -0500

    :card_file_box:Adds pagination for orders index action

--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -24,8 +24,12 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
       as: :json
     assert_response :success
 
-    json_response = JSON.parse(response.body)
-    assert_equal @order.user.orders.count, json_response['data'].count
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    assert_equal @order.user.orders.count, json_response[:data].count
+    assert_not_nil json_response.dig(:links, :first)
+    assert_not_nil json_response.dig(:links, :last)
+    assert_not_nil json_response.dig(:links, :prev)
+    assert_not_nil json_response.dig(:links, :next)
   end
 
   test 'should show order' do
